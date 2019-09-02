806b1c303c0b92f037947ae44d0a8a310f579cc8
romansuhov <romansuhov91@gmail.com>
2019-09-02 17:24:53 +0300

    Update image-viewer.component.tsx

--- src/image-viewer.component.tsx
+++ src/image-viewer.component.tsx
@@ -416,7 +416,7 @@ export default class ImageViewer extends React.Component<Props, State> {
    * 完成布局
    */
   public handleLayout = (event: any) => {
-   if ((event.nativeEvent.layout.width !== _this.width) || (event.nativeEvent.layout.height !== _this.height)) {
+   if ((event.nativeEvent.layout.width !== this.width) || (event.nativeEvent.layout.height !== this.height)) {
       this.hasLayout = true;
 
       this.width = event.nativeEvent.layout.width;
